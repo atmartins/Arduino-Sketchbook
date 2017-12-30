700b66c8af909fe34cf2ad0802bb0ab04463a4f1
Aaron Martins <me@aaronmartins.com>
2017-12-30 09:18:05 -0800

    Clean up and add serial output to receive-side.

--- macsimom_network_protocol_receive/macsimom_network_protocol_receive.ino
+++ macsimom_network_protocol_receive/macsimom_network_protocol_receive.ino
@@ -1,13 +1,24 @@
+// Understanding networking
+//
+// December 2017
+// Nolan Price and Aaron Martins
+//
+// See https://github.com/atmartins/Arduino-Sketchbook/blob/master/README.md
+//
+
 int PIN_IN = 12;
-int PIN_TALK = 13; //show what we received
+int PIN_TALK = 13; //show what we received with on-board LED
 
 void setup() {
   pinMode(PIN_IN, INPUT);
   pinMode(PIN_TALK, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   int pinState = digitalRead(PIN_IN);
   digitalWrite(PIN_TALK, pinState);
-  delay(1);        // delay in between reads for stability
+  Serial.println(pinState);
+  delay(1); // delay in between reads for stability
 }
+
--- macsimom_network_protocol_send/macsimom_network_protocol_send.ino
+++ macsimom_network_protocol_send/macsimom_network_protocol_send.ino
@@ -1,21 +1,26 @@
-int DATA = 13;
-int CLOCK = 12;
-void setup() {
-  
-}
+// Understanding networking
+//
+// December 2017
+// Nolan Price and Aaron Martins
+//
+// See https://github.com/atmartins/Arduino-Sketchbook/blob/master/README.md
+//
 
+int DATA = 13;
+//int CLOCK = 12; //Not used yet
+
+void setup() {}
 
 void sendByte(byte bite) {
   pinMode(DATA, OUTPUT);
-  pinMode(CLOCK, OUTPUT);
+  //pinMode(CLOCK, OUTPUT);
 
-  for(int i = 0; i < 8; i++) {
+  for (int i = 0; i < 8; i++) {
     int binaryVal = bitRead(bite, i);
     digitalWrite(DATA, binaryVal);
     delay(500);
   }
 }
-
 
 void loop() {
   sendByte('M');
@@ -25,10 +30,5 @@ void loop() {
   sendByte('M');
   sendByte('O');
   sendByte('M');
-  
-//  digitalWrite(PIN_OUT, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(4000);                       // wait for a second
-//  digitalWrite(PIN_OUT, LOW);    // turn the LED off by making the voltage LOW
-//  delay(500);
 }
 
